bf1bfbfecfb28d63baa657f292311144b41df25b
Harsh <harshuniverse000@gmail.com>
2022-12-01 14:48:53 +0530

    Added login styles

--- src/api/index.js
+++ src/api/index.js
@@ -47,3 +47,10 @@ export const getPosts = (page = 1, limit = 5) => {
     method: 'GET',
   });
 };
+
+export const login = (email, password) => {
+  return customFetch(API_URLS.login(), {
+    method: 'POST',
+    body: { email, password },
+  });
+};
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,71 @@
+import styles from '../styles/login.module.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useState } from 'react';
+import { login } from '../api/index';
+
 const Login = () => {
-  return <h1>Login Page</h1>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loggingIn, setLoggingIn] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoggingIn(true);
+
+    const response = await login(email, password);
+    if (response.success) {
+      toast.success('Successfully Logged In');
+    } else {
+      toast.error(response.message);
+    }
+    setLoggingIn(false);
+  };
+
+  return (
+    <form className={styles.loginForm} onSubmit={handleSubmit}>
+      <span className={styles.loginSignupHeader}>Log In</span>
+      <div className={styles.field}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          required
+        />
+      </div>
+      <div className={styles.field}>
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          required
+        />
+      </div>
+      <div className={styles.field}>
+        <button disabled={loggingIn}>
+          {loggingIn ? `Logging in...` : `Log In`}
+        </button>
+      </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+    </form>
+  );
 };
 
 export default Login;
