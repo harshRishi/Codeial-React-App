732bb1333553c06f13ece1bd2d5e1ad0a5e3d1d1
Harsh <harshuniverse000@gmail.com>
2022-12-06 17:34:43 +0530

    Private routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,22 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate, Outlet, useLocation } from 'react-router-dom';
 import { useAuth } from '../hooks';
 // import { getPosts } from '../api';
 import { Home, Login, Signup, Settings } from '../pages';
 import { Loader, Navbar } from './';
+
+// making private route
+const PrivateRoute = ({ children, redirectTo }) => {
+  const auth = useAuth(),
+    location = useLocation();
+  if (!auth.user) {
+    // Redirect them to the /login page, but save the current location they were
+    // trying to go to when they were redirected. This allows us to send them
+    // along to that page after they login, which is a nicer user experience
+    // than dropping them off on the home page.
+    return <Navigate to="/Login" state={{ from: location }} />;
+  }
+  return <Outlet />;
+};
 
 // dummy component
 const Page404 = () => {
@@ -22,7 +36,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/register" element={<Signup />} />
-        <Route path="/settings" element={<Settings />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/settings" element=<Settings /> />
+        </Route>
         {/* Incase no route matches render the below route */}
         <Route path="*" element={<Page404 />} />
       </Routes>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,16 +2,18 @@ import styles from '../styles/login.module.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useState } from 'react';
-// import { login } from '../api';
 import { useAuth } from '../hooks'; // our custom hook
-import { useNavigate } from 'react-router-dom';
+// import { Navigate } from 'react-router-dom';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [loggingIn, setLoggingIn] = useState(false);
   const auth = useAuth();
-  const navigate = useNavigate()
+
+  // if (auth.user) {
+  //   return <Navigate to="/Login" />;
+  // }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -20,7 +22,6 @@ const Login = () => {
     const response = await auth.login(email, password);
     if (response.success) {
       toast.success('Successfully Logged In');
-      navigate('/');
     } else {
       toast.error(response.message);
     }
@@ -28,35 +29,37 @@ const Login = () => {
   };
 
   return (
-    <form className={styles.loginForm} onSubmit={handleSubmit}>
-      <span className={styles.loginSignupHeader}>Log In</span>
-      <div className={styles.field}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-          required
-        />
-      </div>
-      <div className={styles.field}>
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-          required
-        />
-      </div>
-      <div className={styles.field}>
-        <button disabled={loggingIn}>
-          {loggingIn ? `Logging in...` : `Log In`}
-        </button>
-      </div>
+    <>
+      <form className={styles.loginForm} onSubmit={handleSubmit}>
+        <span className={styles.loginSignupHeader}>Log In</span>
+        <div className={styles.field}>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            required
+          />
+        </div>
+        <div className={styles.field}>
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            required
+          />
+        </div>
+        <div className={styles.field}>
+          <button disabled={loggingIn}>
+            {loggingIn ? `Logging in...` : `Log In`}
+          </button>
+        </div>
+      </form>
       <ToastContainer
         position="top-right"
         autoClose={3000}
@@ -69,7 +72,7 @@ const Login = () => {
         pauseOnHover
         theme="colored"
       />
-    </form>
+    </>
   );
 };
 
